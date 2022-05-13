6ef6960be7abdccebd0b9669b69e79bdbe2eeb5b
Ella Winters <ellawinters@krbmail.ch>
2022-05-13 07:15:06 -0600

    New Application.

---
+++ sqarerootcalc.py
@@ -0,0 +1,6 @@
+#The Square Root Calculator will find the square root of the number you enter.
+
+#Request input from user
+num = float(input('Enter a number: '))
+sqrt=num**0.5
+print('The square root of %0.2f is %0.2f'%(num,sqrt))
